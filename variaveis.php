ca5376998306cbb04ab3cc4c6a9387c81357d145
Samuel Martins <samuelrmartins17@gmail.com>
2025-03-14 05:43:57 -0300

    test

---
+++ variaveis.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>test dados</title>
+</head>
+<body>
+    <?php
+    $gustavo = 17;
+    $n1 = 5;
+    $n2 = 5;
+    $n3 = $n1 + $n2;
+    $sam = " teste aqui sobre o funcionamento ";
+    echo$gustavo . $n3 . $sam . " //// ";
+    echo $n3;
+    echo$sam;
+    ?>
+
+</body>
+</html>
